6a34d142a94e7fa82593927df5d8bcd1fa8e774d
wxhyzh2005 <wxhyzh2005@163.com>
2024-11-14 21:07:00 +0800

    Update module name.

--- Backup/build/backup.bat
+++ Backup/build/backup.bat
@@ -6,6 +6,6 @@ set current_dir=%~dp0
 
 set current_dir=%current_dir:~0,-1%
 
-java -DrootDir=%current_dir% -Dlog4j.configuration=file:%current_dir%\log4j.properties -jar %current_dir%\SimpleBackup-backup-3.0.0-RELEASE-jar-with-dependencies.jar %*
+java -DrootDir=%current_dir% -Dlog4j.configuration=file:%current_dir%\log4j.properties -jar %current_dir%\simplebackup-backup-3.0.0-RELEASE-jar-with-dependencies.jar %*
 
 endlocal
